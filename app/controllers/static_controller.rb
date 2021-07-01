26d08a45cfd780c2432791cbfaa9cc9b49586559
Katie Secrest <katie.secrest@gmail.com>
2021-07-01 10:46:29 -0600

    not on github?

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,5 @@
 require 'rails/application_controller'
-
-class StaticController < ApplicationController
+class StaticController < Rails::ApplicationController
   layout false
   def index
     render file: Rails.root.join('public', 'index.html')
